68e7fb99f308b20e2f017e33bd62f2d3d37df731
Priyanshu Dhiman <10priyanshudhiman@gmail.com>
2023-10-26 17:23:59 +0530

    feat:Added tests for to check initialEscrow state and fuzz test and normal test for the deposit of ERC20 tokens

---
+++ test/mocks/MockERC20.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+    constructor() ERC20("Mock ERC20", "MRC") {}
+
+    // function to send some specific amount of tokens to address to //
+    function mint(address to, uint256 amount) external {
+        super._mint(to,amount* 10 ** decimals());
+    }
+}
